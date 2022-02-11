970cdf5318359f6bd1473b63deb3de701e9163dc
VIX <55950607+vix-4800@users.noreply.github.com>
2022-02-11 12:31:09 +0400

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -1,3 +1,4 @@
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <cmath>
@@ -6,31 +7,31 @@ using std::cout; using std::endl;
 
 int N = 2;
 
-int* task1(int *src_arr){
+void task1(int *src_arr){
     float distance = 0, tmp = 0;
-    int result[2] = {0,1};
+    int result[2] = {1,2};
 
     for (size_t i = 2; i < N*2; i=i+2) {
         for (size_t j = 0; j < N*2; j=j+2) {
             tmp = sqrt(pow((src_arr[i]-src_arr[j]),2) + pow((src_arr[i+1]-src_arr[j+1]),2));
             if (tmp > distance) {
                 distance = tmp;
-                result[0] = i/2;
+                result[0] = i/2+1;
+                result[1] = j/2+1;
             }
         }
     }
 
-    return result;
+    printf("The biggest distance is between dotes №%i and №%i \n", result[0],result[1]);
 }
 
-int* task2(){
+void task2(){
     int result[3] = {0,1,2};
-    return result;
 }
 
 int main(){
     int dotes[2][4] = {3,4,4,3};
-    cout << task1(*dotes) << endl;
+    task1(*dotes);
 
     return EXIT_SUCCESS;
 }
