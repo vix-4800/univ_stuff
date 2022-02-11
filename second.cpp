2eedcbc8d8fe1d2495d1251b8bef9fa0ad91d92d
VIX <55950607+vix-4800@users.noreply.github.com>
2022-02-11 17:34:18 +0400

    Update second.cpp

--- second.cpp
+++ second.cpp
@@ -1,44 +1,34 @@
+#include <cstdlib>
 #include <iostream>
 
-using std::cout; using std::endl;
+using std::cout;
+using std::endl;
 
-int* combine_bun(int *ar_1, int *ar_2, int t, int N){
-    int res_arr[N];
-    for (size_t i = 0; i < t; i++) {
-        res_arr[i] = ar_1[i];
-    }
-    t = 0;
-    for (size_t i = t; i < N; i++) {
-        res_arr[i] = ar_1[t++];
+const int g_arr_1[] = {1,4,8,11};
+const int g_arr_2[] = {-6,6,8,9};
+
+int* unite(const int* arr_1,const int* arr_2, int* res, const int n){
+    int tmp_1 = 0, tmp_2 = 0;
+
+    for (size_t i = 0; i < n; i++) {
+        if (arr_1[tmp_1] < arr_2[tmp_2])
+            res[i] = arr_1[tmp_1++];
+        else
+            res[i] = arr_2[tmp_2++];
     }
 
-    return res_arr;
+    return res;
 }
 
-int* sort_fun(int* a, int n) {
-   int i, j, min, temp;
-   for (i = 0; i < n - 1; i++) {
-      min = i;
-      for (j = i + 1; j < n; j++)
-      if (a[j] < a[min])
-      min = j;
-      temp = a[i];
-      a[i] = a[min];
-      a[min] = temp;
-   }
-}
+int main() {
+    const int N = sizeof(g_arr_1)/sizeof(int) + sizeof(g_arr_2)/sizeof(int);
+    int result[N];
 
-int main(){
-    int array_1[] = {1,4,8,11};
-    int array_2[] = {-6,6,8,9};
+    unite(g_arr_1, g_arr_2, result, N);
 
-    int N_1 = sizeof(array_1)/sizeof(int);
-    int N_2 = sizeof(array_2)/sizeof(int);
-    int N = N_1 + N_2;
-
-    int* res;
-    res = combine_bun(array_1, array_2, N_1 ,N);
-    cout << sort_fun(res, N) << endl;
+    for (const auto& digit: result)
+        cout << digit << ' ';
+    cout << endl;
 
     return EXIT_SUCCESS;
 }
