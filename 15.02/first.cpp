fb4f0c790f84c462670deaa417f5bca73d96fa47
VIX <55950607+vix-4800@users.noreply.github.com>
2022-02-14 21:06:01 +0400

    Update first.cpp
    added some point for testing purposes, changed variable N from "int" to "const size_t" and modified output in all tasks
    also got rid of some unused modules

--- 15.02/first.cpp
+++ 15.02/first.cpp
@@ -1,9 +1,7 @@
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 #include <cmath>
 
-int N = 2;
+const size_t N = 10;
 
 void task1(int *src_arr){
     float distance = 0, tmp = 0;
@@ -20,16 +18,24 @@ void task1(int *src_arr){
         }
     }
 
-    printf("The biggest distance is between dotes №%i and №%i \n", result[0],result[1]);
+    printf("The biggest distance is between points #%i and #%i \n", result[0],result[1]);
 }
 
-void task2(){
+void task2(int *src_arr){
     int result[3] = {0,1,2};
+    printf("The largest triangle is formed by points #%i, #%i and #%i \n", result[0],result[1],result[2]);
+}
+
+void task3(int *src_arr){
+    int result[4] = {0,1,2,4};
+    printf("The largest square is formed by points #%i, #%i, #%i and #%i \n", result[0],result[1],result[2],result[3]);
 }
 
 int main(){
-    int dotes[2][4] = {3,4,4,3};
+    int dotes[2][10] = {3,4,4,3,8,6,9,0,-3,-5,0,-2,7,-14,-15,9,3,2,6,18};
     task1(*dotes);
+    task2(*dotes);
+    task3(*dotes);
 
     return EXIT_SUCCESS;
 }
