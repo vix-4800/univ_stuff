0195fbb62551d07d77e43d13e97bbc13fc0ab5e7
VIX <55950607+vix-4800@users.noreply.github.com>
2022-04-12 03:01:20 +0400

    js tasks

---
+++ Web projects/JS/Task 2/main.js
@@ -0,0 +1,112 @@
+// const usrName = prompt("Давайте знакомиться.\nКак Вас зовут?");
+
+// if (usrName === null) {
+// 	alert("Ну и ладно...");
+// }
+
+function greetUsr() {
+	if (usrName === null) {
+		alert("Привет, Аноним!");
+	} else {
+		alert("Привет, " + usrName + "!");
+	}
+}
+document.getElementById("greetingBtn").addEventListener("click", greetUsr);
+
+function findMin(a, b) {
+	if (parseInt(a) < parseInt(b)) {
+		return a;
+	} else {
+		return b;
+	}
+}
+
+function squareEquation() {
+	// Дискриминант меньше нуля: 3*x^2 - 4*x + 2 = 0
+	// Дискриминант равен нулю: x^2 - 6*x + 9 = 0
+	// Дискриминант больше нуля: x^2 - 4*x - 5 = 0
+
+	const resDisplay = document.getElementById("square-equation-res");
+	resDisplay.innerHTML = "=>";
+
+	let num1, num2, num3, res;
+	if (
+		document.getElementById("value1").value.trim() &&
+		parseInt(document.getElementById("value1").value.trim()) != 0
+	) {
+		num1 = parseInt(document.getElementById("value1").value);
+	} else {
+		res = "Это не квадратное уравнение!";
+		resDisplay.insertAdjacentText("beforeend", " " + res);
+		return;
+	}
+
+	if (document.getElementById("value2").value.trim()) {
+		num2 = parseInt(document.getElementById("value2").value);
+	} else {
+		num2 = 0;
+	}
+
+	if (document.getElementById("value3").value.trim()) {
+		num3 = parseInt(document.getElementById("value3").value);
+	} else {
+		num3 = 0;
+	}
+
+	let disc = num2 ** 2 - 4 * num1 * num3;
+
+	if (disc < 0) {
+		res = "Корней нет :(";
+	} else if (disc === 0) {
+		res = ((-1 * num2) / 2) * num1;
+	} else {
+		let res1 = ((-1 * num2 + Math.sqrt(disc)) / 2) * num1;
+		let res2 = ((-1 * num2 - Math.sqrt(disc)) / 2) * num1;
+		resDisplay.insertAdjacentText(
+			"beforeend",
+			" x1 = " + res1 + "; x2 = " + res2 + ";"
+		);
+		return;
+	}
+
+	resDisplay.insertAdjacentText("beforeend", " " + res);
+}
+
+function squareEquationClear() {
+	document.getElementById("value1").value = "";
+	document.getElementById("value2").value = "";
+	document.getElementById("value3").value = "";
+	document.getElementById("square-equation-res").innerHTML = "";
+}
+
+// --------------- Calculator --------------- //
+const display = document.querySelector(".display-context");
+const currOperationDisplay = document.querySelector(".current-operation");
+display.innerHTML = "";
+
+function addToDisplay(num) {
+	display.insertAdjacentText("beforeend", num);
+}
+
+function clearDisplay() {
+	display.innerHTML = "";
+}
+
+function changeCurrOperation(symbol) {
+	currOperationDisplay.innerHTML = symbol;
+}
+
+function calcMain() {
+	let currNum = parseInt(display.textContent);
+	let res;
+	switch (currOperation) {
+		case "+":
+			break;
+		case "-":
+			break;
+		case "x":
+			break;
+		case "÷":
+			break;
+	}
+}
