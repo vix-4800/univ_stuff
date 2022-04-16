3401f004696c4e6056af44d4667e6a0f7d9532ad
VIX <55950607+vix-4800@users.noreply.github.com>
2022-04-16 04:45:05 +0400

    task 6 is being done

--- Web projects/JS/Task 6/main.js
+++
@@ -1,5 +0,0 @@
-const inputQuestion1 = document.getElementById("question-1");
-const inputQuestion2 = document.getElementById("question-2");
-const inputQuestion3 = document.getElementById("question-3");
-const inputQuestion4 = document.getElementById("question-4");
-const inputQuestion5 = document.getElementById("question-5");
